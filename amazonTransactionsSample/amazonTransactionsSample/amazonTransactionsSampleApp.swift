5f92d473a6bc604dd56d5b9f5dad6edeea85f7cd
jlozano <jlozano9897@gmail.com>
2024-03-17 13:34:35 -0700

    Amazon transactions demo

---
+++ amazonTransactionsSample/amazonTransactionsSample/amazonTransactionsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  amazonTransactionsSampleApp.swift
+//  amazonTransactionsSample
+//
+//  Created by juan lozano on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct amazonTransactionsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
